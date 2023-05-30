2dde1cae82b460fcfd1d4a941e2f63d621f71a61
Bakhtovar2004 <mamurovbakhtovar721@gmail.com>
2023-05-30 16:49:32 +0500

    list-users-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../components/Header.css'
+export default function Header() {
+  return (
+    <div className='header'>
+<h1>Список пользователей</h1>
+    </div>
+  )
+}
---
+++ src/components/user/User.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import './User.css';
+
+export default function User({ user, onDelete, onEdit }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedUser, setEditedUser] = useState(user);
+
+  const handleDelete = () => {
+    onDelete(user.id);
+  };
+
+  const handleToggleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleSave = () => {
+    onEdit(user.id, editedUser);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setEditedUser(user);
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  if (isEditing) {
+    return (
+      <div>
+        <div className="cardEdit">
+          <img src={editedUser.img} alt="" />
+          <input
+            type="text"
+            name="name"
+            value={editedUser.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="age"
+            value={editedUser.age}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="work"
+            value={editedUser.work}
+            onChange={handleInputChange}
+          />
+         <div className='btnEdit'>
+         <button onClick={handleSave}>Сохранить</button>
+          <button onClick={handleCancel}>Отмена</button>
+         </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="card">
+        <img src={user.img} alt="" />
+        <h2>{user.name}</h2>
+        <h3>{user.age} year old</h3>
+        <h3>{user.work}</h3>
+        <button onClick={handleDelete}>Удалить</button>
+        <button onClick={handleToggleEdit}>Изменить</button>
+      </div>
+    </div>
+  );
+}
